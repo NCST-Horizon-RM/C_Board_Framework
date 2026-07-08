0d6cdbcba9548e506ee127b28e8fb40217411043
7yuesx <3196873451@qq.com>
2026-07-09 01:46:53 +0800

    修改麦轮和全向轮解算

--- User/Algorithm/Kinematics/Chassis_Calc.h
+++ User/Algorithm/Kinematics/Chassis_Calc.h
@@ -8,44 +8,25 @@
 
 typedef struct
 {
-    float wheel_perimeter;    /* 轮的周长（mm）*/
-    float wheeltrack;         /* 轮距（mm）*/
-    float wheelbase;          /* 轴距（mm）*/
-    float rotate_x_offset;    /* 相对于底盘中心的x轴旋转偏移量(mm) */
-    float rotate_y_offset;    /* 相对于底盘中心的y轴旋转偏移量(mm) */
+    float wheel_r;    /* 轮的半径（m）*/
+    float chassis_r;         /* 底盘旋转半径（m）*/
+    float phi[4];          /* 轮子安装方位角 (rad)*/
     float deceleration_ratio; /* 电机减速比 */
-    int max_vx_speed;         /* 底盘的x轴的最大速度(mm/s) */
-    int max_vy_speed;         /* 底盘的y轴的最大速度(mm/s) */
-    int max_vw_speed;         /* 底盘的自转的最大速度(degree/s) */
-    int max_wheel_ramp;       /* 3508最大转速 */
-    float raid_fr;            // 右前
-    float raid_fl;            // 左前
-    float raid_bl;            // 左后
-    float raid_br;            // 右后
-    float wheel_rpm_ratio;    // 用来将速度转化成转每分钟
 } mecanumInit_typdef;
 
 typedef struct
 {
-    float wheel_perimeter;    /* 轮的周长（mm）*/
-    float CHASSIS_DECELE_RATIO; /* 电机减速比 */
-    float LENGTH_A;         /* 底盘长的一半（mm）*/
-    float LENGTH_B;          /* 底盘宽的一半（mm）*/
-    float rotate_radius;     /* (A+B)/57.3, 将角速度(deg/s)转换为线速度(mm/s) */
-    float wheel_rpm_ratio;   /* 线速度(mm/s) -> 轮速(rpm) */
-    int max_vx_speed;        /* 底盘x轴最大速度(mm/s) */
-    int max_vy_speed;        /* 底盘y轴最大速度(mm/s) */
-    int max_vw_speed;        /* 底盘自转最大速度(deg/s) */
-    int max_wheel_ramp;      /* 3508最大转速 */
+    float wheel_r;    /* 轮的半径（m）*/
+    float chassis_r;         /* 底盘旋转半径（m）*/
+    float phi[4];          /* 轮子安装方位角 (rad)*/
+    float deceleration_ratio; /* 电机减速比 */
 } OmniInit_typdef;
 
-extern OmniInit_typdef OmniInit_t;
+uint8_t Mecanum_Init(mecanumInit_typdef *mecanumInitT);
+void Mecanum_Calc(float *wheel_rpm, float vx_temp, float vy_temp, float vr, mecanumInit_typdef *mecanumInit_t);
 
-uint8_t MecanumInit(mecanumInit_typdef *mecanumInitT);
-void MecanumResolve(float *wheel_rpm, float vx_temp, float vy_temp, float vr, mecanumInit_typdef *mecanumInit_t);
-
-uint8_t OmniInit(OmniInit_typdef *OmniInitT);
-void Omni_calc(float *wheel_rpm, float vx_temp, float vy_temp, float vr, OmniInit_typdef *OmniInit_t);
+uint8_t Omni_Init(OmniInit_typdef *OmniInit_t);
+void Omni_Calc(float *wheel_rpm, float vx_temp, float vy_temp, float vr, OmniInit_typdef *OmniInit_t);
 
 
 /* ==================== 舵轮 ==================== */
@@ -113,6 +94,5 @@ void Swerve_Inverse_Calc(Swerve_Command_t *cmd, Swerve_State_t *state,
                          float vx, float vy, float vw,
                          const Swerve_Feedback_t *fb);
 
-float CHASSIS_GET_MAX_TARGET(float MAX_POWER);
 
 #endif //G4_FRAMEWORK_CHASSIS_CALC_H
