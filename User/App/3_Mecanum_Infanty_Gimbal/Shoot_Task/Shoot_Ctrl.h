79bf1383d6c49aa3c2d0d18c9084bde8eeb2f71d
Rxy <R060219@outlook.com>
2026-07-11 15:01:46 +0800

    步兵云台添加完成，发射待完成

--- User/App/3_Mecanum_Infanty_Gimbal/Shoot_Task/Shoot_Ctrl.h
+++ User/App/3_Mecanum_Infanty_Gimbal/Shoot_Task/Shoot_Ctrl.h
@@ -4,5 +4,20 @@
 
 #ifndef SHOOT_CTRL_H
 #define SHOOT_CTRL_H
+#include <stdint.h>
+#include "Robot_Config.h"
+#include "Classic_Control.h"
+typedef struct {
+    PID_t Bmotor_P;
+    PID_t Bmotor_S;
+    PID_t Lfire_S;
+    PID_t Rfire_S;
+} Shoot_Ctrl_Block_t;
+
+uint8_t Shoot_Control_Init(void);
+void Shoot_Control_Task(const Shoot_Motor_Group_t *g_motor);
+void Single_Shoot_Control(const Shoot_Motor_Group_t *g_motor);
+void Smooth_Shoot_Control(const Shoot_Motor_Group_t *g_motor);
+void Usmooth_Shoot_Control(const Shoot_Motor_Group_t *g_motor);
 
 #endif //SHOOT_CTRL_H
