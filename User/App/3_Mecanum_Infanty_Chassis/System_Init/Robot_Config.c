46ec77399b988c80f77ab063de682bcc7cd3c14f
Cao Kangqi <1822776173@qq.com>
2026-07-14 23:17:41 +0800

    修改串口BSP，增加修改波特率功能

--- User/App/3_Mecanum_Infanty_Chassis/System_Init/Robot_Config.c
+++ User/App/3_Mecanum_Infanty_Chassis/System_Init/Robot_Config.c
@@ -16,13 +16,13 @@
 Chassis_Motor_Group_t chassis_motors;
 Gimbal_Motor_Group_t  gimbal_motors;
 
-UART_RX_NODE(&huart3, 18, DBUS_RX_DATA, NULL, 18, &DBUS, DBUS_Resolved);
+UART_RX_NODE(&huart3,100000 ,18, DBUS_RX_DATA, NULL, 18, &DBUS, DBUS_Resolved);
 OFFLINE_NODE(&DBUS.offline, DBUS_OFFLINE_TIME, GROUP_NONE);
 
-UART_RX_NODE(&huart1, 0, VT13_RX_DATA, NULL, 13, &VT13, VT13_Resolved);
+UART_RX_NODE(&huart1,921600 ,21, VT13_RX_DATA, NULL, 21, &VT13, VT13_Resolved);
 OFFLINE_NODE(&VT13.offline, DBUS_OFFLINE_TIME, GROUP_NONE);
 
-UART_RX_NODE(&huart6, 0, Referee_Rx_Buf[0], Referee_Rx_Buf[1], REFEREE_RXFRAME_LENGTH, NULL, Referee_System_Frame_Update);
+UART_RX_NODE(&huart6,115200 ,0, Referee_Rx_Buf[0], Referee_Rx_Buf[1], REFEREE_RXFRAME_LENGTH, NULL, Referee_System_Frame_Update);
 OFFLINE_NODE(&Referee.offline, REFEREE_OFFLINE_TIME, GROUP_NONE);
 
 CAN_RX_NODE(CAN1, 0x288, &cap, Power_Cap_Rx);
